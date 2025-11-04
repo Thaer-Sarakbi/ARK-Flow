fc6b90f9563ce876dd1a6fc9bd34f4181a91f9c9
Thaer-Sarakbi <thaersarakbi@gmail.com>
2025-11-04 11:15:09 +0800

    initialize app

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default {
+    expo: {
+      name: 'ark-flow',
+      slug: 'ark-flow',
+      web: {
+        output: 'single'
+      }
+    }
+  };
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// screens/ProfileScreen.tsx
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      
+    </SafeAreaProvider>
+  );
+}
